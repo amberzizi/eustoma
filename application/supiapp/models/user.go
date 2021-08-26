a403952c1f7e86a40cb7e5705e35d82e6e2d6880
amberhu <amberoracle@163.com>
2021-08-26 13:53:39 +0800

    supi-models

---
+++ application/supiapp/models/user.go
@@ -0,0 +1,75 @@
+package models
+
+import (
+	"errors"
+)
+
+//统一使用的常量名称
+const ContextUserIdKey = "user_id" //用户登录后上下文携带的user_id
+
+//定义自定义error
+var (
+	ErrorUserExist         = errors.New("用户已存在")
+	ErrorUserNotExist      = errors.New("用户名不存在")
+	ErrorUserPassowrdWrong = errors.New("用户密码错误")
+)
+
+//完整用户信息
+type User struct {
+	Id          int64 `json:"id,string"`
+	User_id     int64 `json:"user_id,string"`
+	Username    string
+	Password    string
+	Salt        string
+	Email       string
+	Gender      int
+	Create_time string
+	Update_time string
+}
+
+//外部可用的用户信息
+type Userinfopublic struct {
+	Id       int64 `json:"id,string"`
+	User_id  int64 `json:"user_id,string"`
+	Username string
+	//Password    string
+	//Salt        string
+	Email       string
+	Gender      int
+	Create_time string
+	Update_time string
+}
+
+//用来登录鉴别的user信息
+type Userforlogin struct {
+	Id       int64 `json:"id,string"`
+	User_id  int64 `json:"user_id,string"`
+	Username string
+	Password string
+	Salt     string
+	//Email       string
+	//Gender      int
+	//Create_time string
+	//Update_time string
+}
+
+//用来生成jwt的信息
+type Userforjwt struct {
+	User_id  int64 `json:"user_id,string"`
+	Username string
+}
+
+func (u User) TableName() string {
+	return "user"
+}
+
+func (u Userinfopublic) TableName() string {
+	return "user"
+}
+
+func (u Userforlogin) TableName() string {
+	return "user"
+}
+func (u Userforjwt) TableName() string {
+	return "user"
+}
